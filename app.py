188df69ff428bcf67fb12a05d9e11df000009f1f
Shaurya <srswarup_b21@ce.vjti.ac.in>
2023-04-13 01:13:59 +0530

    Added queries

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PORT'] = 3310
-app.config['MYSQL_PASSWORD'] = 'Sharayu@2000'
+# app.config['MYSQL_PORT'] = 3310
+app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'spam_alert_system'
 
 mysql = MySQL(app)
@@ -17,7 +17,7 @@ mysql = MySQL(app)
 
 filename = "smsmodel.pkl"
 with open(filename, 'rb') as f:
-    model, tokenizer = pickle.load(f)
+    smsmodel, tokenizer = pickle.load(f)
 
 with open('emailmodel.pkl', 'rb') as f:
     clf = pickle.load(f)
@@ -28,7 +28,7 @@ def predictSMS(text_data):
     tokenized_data = tokenizer(text_data, padding=True, return_tensors="pt")
 
     # Pass the tokenized data through the model and get the predicted labels
-    output_prediction = model(**tokenized_data)
+    output_prediction = smsmodel(**tokenized_data)
     numpy_output = output_prediction.logits.detach().numpy()
     preds = np.argmax(numpy_output, axis=-1)
 
@@ -43,10 +43,6 @@ def predictEmail(emails):
     return list(predictions)
 
 
-text_data = "Congratulations! You have been selected as a winner. Text WIN to 55555 to claim your prize."
-predicted_labels = predictSMS(text_data)
-# print(predicted_labels)
-
 # This is the usage of the email part
 email = {
     "emailId": "example@example.com",
@@ -56,15 +52,118 @@ sms = {
     "phoneNo": "1234567890",
     "content": "Congratulations! You have been selected as a winner. Text WIN to 55555 to claim your prize."
 }
-predictions = predictEmail(email)
-# print(predictions)
 
 
-@app.route('/emails', methods=['GET', 'POST'])
-def emails():
+# 1. Get request -- get spam count from database
+
+
+@app.route('/phoneSpamCount/<int:phone_no>', methods=['POST', 'GET'])
+def phoneSpamCount(phone_no):
+    cur = mysql.connection.cursor()
+    query = "SELECT up_votes, down_votes FROM phone_calls WHERE phone_no = %s"
+    cur.execute(query, (phone_no,))
+    result = cur.fetchone()
+    # Check if the phone number was found in the database
+    if result is None:
+        cur.execute("INSERT INTO phone_calls VALUES (%s, %s, %s)",
+                    (phone_no, 0, 0))
+        mysql.connection.commit()
+        return jsonify({"error": "Phone number not found"})
+    # for value in result:
+    up_votes, down_votes = result
+    spam_risk = 0
+    # Create a JSON response with the up_votes and down_votes data
+    response = {
+        "phone_no": phone_no,
+        "up_votes": up_votes,
+        "down_votes": down_votes,
+        "spam_risk": spam_risk
+    }
+    try:
+        spam_risk = (down_votes/(down_votes+up_votes))
+        return jsonify(response)
+    except:
+        spam_risk = 0
+        return jsonify(response)
+
+
+# 2. Post request -- post vote to database
+
+
+@app.route('/phoneCallVote/<int:phone_no>/vote', methods=['POST', 'GET'])
+def phoneCallVote(phone_no):
+    upvote = request.args.get('upvote')
+    # downvote = request.args.get('downvote')
+
+    cur = mysql.connection.cursor()
+    query = "SELECT up_votes, down_votes FROM phone_calls WHERE phone_no = %s"
+    cur.execute(query, (phone_no,))
+    result = cur.fetchone()
+
+    # Check if the phone number was found in the database
+    if result is None:
+        return jsonify({"error": "Phone number not found"})
+
+    up_votes, down_votes = result
+
+    if upvote == '1':
+        up_votes += 1
+    elif upvote == '-1':
+        down_votes += 1
+
+    # Update the database with the new vote counts
+    update_query = "UPDATE phone_calls SET up_votes=%s, down_votes=%s WHERE phone_no=%s"
+    cur.execute(update_query, (up_votes, down_votes, phone_no))
+
+    # Commit the changes to the database
+    mysql.connection.commit()
+
+    spam_risk = (down_votes/(down_votes+up_votes))
+    # Create a JSON response with the up_votes and down_votes data
+    response = {
+        "phone_no": phone_no,
+        "up_votes": up_votes,
+        "down_votes": down_votes,
+        "spam_risk": spam_risk
+    }
+
+    return jsonify(response)
+
+
+@app.route('/email', methods=['POST'])
+async def email():
     data = request.get_json()
-    content = data['content']
-    result = predictEmail(content)
+    content = []
+    content.insert(0, data['content'])
+    prediction = predictEmail(content)
+    print(prediction)
+    email_id = data['emailId']
+
+    cur = mysql.connection.cursor()
+    cur.execute(
+        "SELECT email_id FROM email where email_id = %s", (email_id,))
+
+    senderData = cur.fetchone()
+
+    if senderData is None:
+        cur.execute(
+            "INSERT INTO email (email_id) VALUES (%s)", (email_id,))
+    cur.execute(
+        "SELECT email_id, no_of_reports,total_invokations FROM email where email_id = %s", (email_id,))
+    senderData = cur.fetchone()
+    email_id, no_of_reports, total_invokations = senderData
+    if prediction[0] == 1:
+        no_of_reports += 1
+    total_invokations += 1
+    update_query = "UPDATE email SET no_of_reports=%s, total_invokations=%s WHERE email_id=%s"
+    cur.execute(update_query, (no_of_reports,
+                               total_invokations, email_id,))
+    mysql.connection.commit()
+    result = {
+        "isSpam": int(prediction[0]),
+        "no_of_reports": no_of_reports,
+    }
+    cur.close()
     return jsonify(result)
 
 
@@ -78,8 +177,3 @@ def sms():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-# emails --> SEND EMAIL TEXT TO ML MODEL
-# sms --> SEND SMS TEXT TO ML MODEL
-# call --> QUERY PHONE NO.  --> IF IT IS THERE IN DATABASE --> CHECK SPAM VOTES AND SEND RISK SCORE JSON
-# IF NOT THERE IN DATABASE --> ADD PHONE NO. --> THEN SEND "Unknown Number"
